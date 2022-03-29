6d8cf67983a262fe3504c46c14dbdcf48913d011
Rifat Hossain <rifat.hossain.dhrubo@gmail.com>
2022-03-29 12:57:55 +0600

    feat(aboutUs): added about us page

---
+++ apps/web/pages/about.tsx
@@ -0,0 +1,143 @@
+import React from "react";
+import HomePageLayout from "../components/Layout/HomePageLayout";
+
+const About = () => {
+  return (
+    <>
+      <header>
+        <div className="container">
+          <div className="shape" id="moon"></div>
+          <h1 className="big-title mb-5">Welcome</h1>
+          <div className="col-md-8 col-xl-8 col-lg-8 mx-auto">
+            <p className="text-center">
+              TikStock&apos;s story began during lockdown, when the green
+              portfolios of successful traders started turning red. Now unsure
+              of which direction the market was headed, we were left with an
+              unsettling feeling….
+              <br />
+              A feeling that made us question how many others were forced into
+              the same dilemma. As many retail investors entered the market, our
+              team here at TikStock dedicated themselves to the creation of this
+              platform. A platform that allows users to gain key insights into
+              their favorite stocks, cryptos, indexes and more.
+              <br />
+              Not only have we&apos;ve partnered with award winning fintech
+              leaders to provide users with top market analytics, but we&apos;ve
+              made it our focus to deliver this information to you with an
+              attractive design at an affordable price.
+            </p>
+          </div>
+        </div>
+      </header>
+      <section className="animate-container">
+        <div className="floating1"></div>
+        <div className="floating2"></div>
+      </section>
+      <section className="about">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-8 col-lg-8 col-xl-8 col-sm-12 mx-auto">
+              <div className="row">
+                <div className="col-md-6 col-lg-6 col-xl-6 col-sm-6 text-center height-adjust">
+                  <div
+                    className="shape shape-lg"
+                    style={{
+                      top: "5%",
+                      background: "transparent",
+                    }}
+                    id="anim-position-1"
+                  ></div>
+                  <h3>American Owned.</h3>
+                  <p>
+                    TikStock is a proud, American owned company founded with the
+                    intention to provide stock information to traders and
+                    investors in an understandable way.
+                  </p>
+                </div>
+              </div>
+              <div className="row d-flex  justify-content-end">
+                <div className="col-md-6 col-lg-6 col-xl-6 col-sm-6 text-center align-self-end height-adjust">
+                  <div
+                    className="shape shape-lg"
+                    style={{
+                      top: "5%",
+                      left: "60%",
+                      background: "transparent",
+                    }}
+                    id="anim-position-2"
+                  ></div>
+                  <h3>
+                    We are traders Just <br /> Like you
+                  </h3>
+                  <p>
+                    As traders ourselves who studied books, watched videos and
+                    took expensive online courses in attempts to learn the
+                    secrets to market conquering, we took to the interne to find
+                    an affordable and comprehensive alternative. A platform
+                    where beginners can go to dive deep without feeling
+                    overwhelmed. We failed.
+                  </p>
+                </div>
+              </div>
+
+              <div className="row">
+                <div className="col-md-6 col-lg-6 col-xl-6 col-sm-6 text-center height-adjust">
+                  <div
+                    className="shape shape-lg"
+                    style={{
+                      top: "5%",
+                      left: "35%",
+                      background: "transparent",
+                    }}
+                    id="anim-position-3"
+                  ></div>
+                  <h3>TikStock&apos;s Creation.</h3>
+                  <p>
+                    That&apos;s when we decided to take matters into our own
+                    hands and create one for you. Our mission here at TikStock
+                    is to make sure you&apos;re having fun while learning. To
+                    make sure you have a platform that doesn&apos;t ruin you
+                    motivation or confidence through information overload, but
+                    boosts it through user-friendly interfaces and simplistic
+                    versatility.
+                  </p>
+                </div>
+              </div>
+              <div className="row d-flex  justify-content-end mb-5">
+                <div className="col-md-6 col-lg-6 col-xl-6 col-sm-6 text-center align-self-end height-adjust">
+                  <div
+                    className="shape shape-lg"
+                    style={{
+                      top: "5%",
+                      left: "20%",
+                      background: "transparent",
+                    }}
+                    id="anim-position-4"
+                  ></div>
+                  <h3>Let us know</h3>
+                  <p>
+                    So, if for some reason we have failed at doing this, fill
+                    out the contact form found at the Help Center and let us
+                    know how we can improve. We made TikStock with you in mind
+                    and want to make sure you achieve your financial goals.
+                  </p>
+                </div>
+              </div>
+              <div className="row">
+                <h1 className="text-center mt-5 mb-5">
+                  Thank you for your support
+                </h1>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+About.getLayout = function getLayout(page: React.ReactElement) {
+  return <HomePageLayout>{page}</HomePageLayout>;
+};
+
+export default About;
