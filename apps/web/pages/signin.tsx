a7c0f786dbb8d6d9861f8c57d0ddc704ca21969b
Rifat Hossain <rifat.hossain.dhrubo@gmail.com>
2022-03-30 16:15:05 +0600

    feat(signIn): added signin template

--- apps/web/pages/signin.tsx
+++ apps/web/pages/signin.tsx
@@ -6,6 +6,7 @@ import {
 import React from "react";
 import { useForm } from "react-hook-form";
 import { useMutation } from "react-query";
+import HomePageLayout from "../components/Layout/HomePageLayout";
 import {
   clientSignInWithEmailAndPassword,
   clientSignInWithGoogle,
@@ -39,10 +40,15 @@ const SignIn = () => {
     }
   );
   const onSubmit = (data: FormType) => {
-    mutate({
-      email: data.email,
-      password: data.password,
-    });
+    console.log("🚀 ~ file: signin.tsx ~ line 43 ~ onSubmit ~ data", data);
+    // mutate({
+    //   email: data.email,
+    //   password: data.password,
+    // });
+  };
+
+  const onError = (error: any) => {
+    console.log("🚀 ~ file: signin.tsx ~ line 51 ~ onError ~ error", error);
   };
 
   const handleGoogleSignIn = () => {
@@ -50,24 +56,84 @@ const SignIn = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <input
-        type="text"
-        placeholder="Email"
-        {...register("email", { required: true, pattern: /^\S+@\S+$/i })}
-      />
-      <input
-        type="password"
-        placeholder="Password"
-        {...register("password", { required: true })}
-      />
-
-      <button type="submit">Sign in</button>
-      <button onClick={handleGoogleSignIn} type="button">
-        Google
-      </button>
-    </form>
+    <>
+      <header
+        style={{
+          minHeight: 700,
+        }}
+      >
+        <div className="container">
+          <div className="shape" id="moon"></div>
+          <h1 className="big-title mb-5">Sign in</h1>
+          <div className="col-md-5 col-xl-5 col-lg-5 mx-auto signup-container">
+            <form
+              className="authenticate"
+              onSubmit={handleSubmit(onSubmit, onError)}
+              id="signin-form"
+            >
+              <div className="form-group">
+                <label htmlFor="email" className="form-label">
+                  Email
+                </label>
+                <input
+                  type="email"
+                  className="form-control"
+                  placeholder="i.e - johndoe@gmail.com"
+                  {...register("email", {
+                    required: {
+                      value: true,
+                      message: "Email is required",
+                    },
+                  })}
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="password" className="form-label">
+                  Password
+                </label>
+                <input
+                  type="password"
+                  className="form-control"
+                  placeholder="********"
+                  {...register("password", {
+                    required: {
+                      value: true,
+                      message: "Password is required",
+                    },
+                  })}
+                />
+              </div>
+            </form>
+            <div className="text-center btn-holder">
+              <button type="submit" className="btn-theme" form="signin-form">
+                Sign in
+              </button>
+            </div>
+            <p className="text-center mt-5">
+              Don&apos;t have an account?{" "}
+              <a
+                href="/signup"
+                style={{
+                  color: "#02d9ff",
+                  fontWeight: 600,
+                }}
+              >
+                Sign up
+              </a>
+            </p>
+          </div>
+        </div>
+      </header>
+      <section className="animate-container">
+        <div className="floating1"></div>
+        <div className="floating2"></div>
+      </section>
+    </>
   );
+};
+
+SignIn.getLayout = function getLayout(page: React.ReactElement) {
+  return <HomePageLayout>{page}</HomePageLayout>;
 };
 
 export const getServerSideProps = withAuthUserTokenSSR({
